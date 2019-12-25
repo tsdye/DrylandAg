be8779dd91cd0640f24426f8cdabee772e3b210e
Thomas S. Dye <tsd@tsdye.online>
2019-12-25 11:03:05 -1000

    Working version of annual summaries
    Quite a bit of code duplicated from monthly summaries.

--- R/annual_summaries.R
+++ R/annual_summaries.R
@@ -2,13 +2,18 @@ plot_annual_summaries <- function(data = NULL,
                                   input_file_name = NULL,
                                   output_dir = getwd(),
                                   annual_summary_file = "annual_summary_file.pdf",
-                                  legend_order = NULL,
-                                  palette = NULL,
+                                  order_legend = FALSE,
+                                  palette = "bright",
+                                  default_palette = "viridis",
+                                  png_dpi =120,
                                   verbose = TRUE) {
 
     library('ggplot2')
 
     ## set up
+    palettes <- c("bright", "contrast", "vibrant", "muted")
+    default_palettes <- c("viridis", "plasma", "inferno")
+
     if(is.null(data) & is.null(input_file_name))
         stop("One of 'data' and 'input_file_name' required")
 
@@ -22,14 +27,24 @@ plot_annual_summaries <- function(data = NULL,
     if(!is.null(input_file_name))
         data <- readRDS(file = input_file_name)
 
+    if(!is.element(palette, palettes))
+        stop(sprintf("Palette '%s' is not recognized", palette))
+
+    if(!is.element(default_palette, default_palettes))
+        stop(sprintf("Default palette '%s' is not recognized", default_palette))
+
     max_colors <- c(bright = 7, contrast = 3, vibrant = 7, muted = 9)
 
-    if(!is.null(palette))
-        if(length(data$rasters) > max_colors[palette])
-            stop(sprintf("Requested %i colors but %s offers only %i",
-                         length(data$rasters), palette, max_colors[palette]))
-        else
-            message(sprintf("Preparing color output using the %s palette", palette))
+    if(length(data$rasters) > max_colors[palette])
+        use_grey <- TRUE
+    else
+        use_grey <- FALSE
+
+    if (use_grey == TRUE)
+        message(sprintf("Requested %i colors but '%s' palette offers %i. Using default palette, '%s'.",
+                        length(data$rasters), palette, max_colors[palette], default_palette))
+    else
+        message(sprintf("Preparing color output using the %s palette", palette))
 
     color_plot_height <- c(15, 4, 6)
     min_plot_size <- 7 / 2
@@ -48,14 +63,15 @@ plot_annual_summaries <- function(data = NULL,
     }
 
     ## function for plotting comparative graphs
-    comparative_graph <- function(data, out_file, palette = NULL,
+    comparative_graph <- function(data, out_file, palette,
+                                  default_palette,
                                   height = 7, width = 7) {
-        if(is.null(palette)) {
+        if(use_grey == TRUE) {
             p <- ggplot(data = data, aes(x = polygon_name, y = value, fill = polygon_name))
-            p <- p + geom_boxplot(outlier.shape = 20, alpha = 0.4)
-            p <- p + scale_fill_grey()
-            ## p <- p + guides(fill = FALSE)
-            p <- p + labs(y = "Value", x = NULL, fill = "Name")}
+            p <- p + geom_boxplot(outlier.shape = 20)
+            p <- p + scale_fill_viridis_d(option = default_palette)
+            ## p <- p + guides(fill = guide_legend(nrow = 5))
+            p <- p + labs(y = "Value", x = NULL, fill = "Place")}
         else {
                 p <- ggplot(data = data, aes(x = value,
                                              fill = polygon_name,
@@ -72,9 +88,12 @@ plot_annual_summaries <- function(data = NULL,
                                 "bright" = khroma::scale_fill_bright(),
                                 "vibrant" = khroma::scale_fill_vibrant(),
                                 "muted" = khroma::scale_fill_muted())
-                p <- p + labs(y = "Density", x = NULL, fill = "Name")}
+                p <- p + labs(y = "Density", x = NULL, fill = "Place")}
+
         p <- p + theme_bw()
-        if(is.null(palette)) {
+        p <- p + theme(legend.position="bottom")
+
+        if(use_grey == TRUE) {
             p <- p + theme(axis.ticks.x = element_blank(),
                            axis.text.x = element_blank())
             p <- p + theme(panel.grid.minor.x=element_blank(),
@@ -84,19 +103,39 @@ plot_annual_summaries <- function(data = NULL,
         else {
             p <- p + facet_wrap(~plot_title, ncol = 2, scales = "free")
         }
-        ggsave(filename = out_file, plot = p, height = height, width = width)
+
+        switch(tools::file_ext(out_file),
+               "pdf" = ggsave(filename = out_file,
+                              plot = p,
+                              height = height,
+                              width = width,
+                              limitsize = FALSE,
+                              device = cairo_pdf),
+               "png" = ggsave(filename = out_file,
+                              plot = p,
+                              height = height,
+                              width = width,
+                              dpi = png_dpi,
+                              limitsize = FALSE,
+                              type = "cairo"),
+               ggsave(filename = out_file,
+                      plot = p,
+                      height = monthly_plot_size,
+                      width = monthly_plot_size,
+                      limitsize = FALSE))
+
     }
 
     ## Plot annual metrics across areas of interest
 
     out_file <- filename_splice(output_dir, annual_summary_file, "_1")
-    out_height <- if(is.null(palette))
+    out_height <- if(use_grey == TRUE)
                       min_plot_size * 4
                   else
                       color_plot_height[1]
 
-    out_width <- if(is.null(palette))
-                     min_plot_size * 4
+    out_width <- if(use_grey == TRUE)
+                     min_plot_size * 5
                  else
                      7
 
@@ -121,21 +160,38 @@ plot_annual_summaries <- function(data = NULL,
         comb_dfs <- dplyr::bind_rows(comb_dfs, dfs)}
 
     ## Possibly order the legend labels
-    if(!is.null(legend_order))
-        comb_dfs$polygon_name <- factor(comb_dfs$polygon_name,
-                                        levels = legend_order)
+    if(order_legend == TRUE)
+        if(!is.null(data$sort_order)) {
+            temp_df <- data.frame(n = data$polygon_names,
+                                  o = data$sort_order)
+            ordered_names <- temp_df[order(temp_df$o),]$n
+            comb_dfs$polygon_name <- factor(comb_dfs$polygon_name,
+                                            levels = ordered_names)}
+        else
+            message(sprintf("Legend sort order is not specified in %s",
+                            if(is.null(input_file_name))
+                                "input data"
+                            else
+                                input_file_name))
 
-
-    comparative_graph(data = comb_dfs, palette = palette,
-                      out_file = out_file, height = out_height,
+    comparative_graph(data = comb_dfs,
+                      out_file = out_file,
+                      palette = palette,
+                      default_palette = default_palette,
+                      height = out_height,
                       width = out_width)
 
     ## wind metrics
     out_file <- filename_splice(output_dir, annual_summary_file, "_2")
-    out_height <- if(is.null(palette))
+    out_height <- if(use_grey == TRUE)
                       7
                   else
                       color_plot_height[2]
+
+    out_width <- if(use_grey == TRUE)
+                     min_plot_size * 3
+                 else
+                     7
 
     if(verbose == TRUE)
         message(sprintf("Writing annual wind metrics to %s", out_file))
@@ -159,23 +215,37 @@ plot_annual_summaries <- function(data = NULL,
         comb_dfs <- dplyr::bind_rows(comb_dfs, dfs)}
 
     ## Possibly order the legend labels
-    if(!is.null(legend_order))
-        comb_dfs$polygon_name <- factor(comb_dfs$polygon_name,
-                                        levels = legend_order)
+    if(order_legend == TRUE)
+        if(!is.null(data$sort_order)) {
+            temp_df <- data.frame(n = data$polygon_names,
+                                  o = data$sort_order)
+            ordered_names <- temp_df[order(temp_df$o),]$n
+            comb_dfs$polygon_name <- factor(comb_dfs$polygon_name,
+                                            levels = ordered_names)}
+        else
+            message(sprintf("Legend sort order is not specified in %s",
+                            if(is.null(input_file_name))
+                                "input data"
+                            else
+                                input_file_name))
 
-    comparative_graph(data = comb_dfs, palette = palette,
-                      out_file = out_file, height = out_height)
+    comparative_graph(data = comb_dfs,
+                      palette = palette,
+                      default_palette = default_palette,
+                      out_file = out_file,
+                      height = out_height,
+                      width = out_width)
 
     ## intra-annual rainfall metrics
 
     comb_dfs <- NULL
     out_file <- filename_splice(output_dir, annual_summary_file, "_3")
-    out_height <- if(is.null(palette))
+    out_height <- if(use_grey == TRUE)
                       7
                   else
                       color_plot_height[3]
-    out_width <- if(is.null(palette))
-                     min_plot_size * 3
+    out_width <- if(use_grey == TRUE)
+                     min_plot_size * 4
                  else
                      7
 
@@ -217,10 +287,23 @@ plot_annual_summaries <- function(data = NULL,
         comb_dfs <- dplyr::bind_rows(comb_dfs, dfs)}
 
     ## Possibly order the legend labels
-    if(!is.null(legend_order))
-        comb_dfs$polygon_name <- factor(comb_dfs$polygon_name,
-                                        levels = legend_order)
+    if(order_legend == TRUE)
+        if(!is.null(data$sort_order)) {
+            temp_df <- data.frame(n = data$polygon_names,
+                                  o = data$sort_order)
+            ordered_names <- temp_df[order(temp_df$o),]$n
+            comb_dfs$polygon_name <- factor(comb_dfs$polygon_name,
+                                            levels = ordered_names)}
+        else
+            message(sprintf("Legend sort order is not specified in %s",
+                            if(is.null(input_file_name))
+                                "input data"
+                            else
+                                input_file_name))
 
-    comparative_graph(data = comb_dfs, palette = palette,
-                      out_file = out_file, height = out_height,
+    comparative_graph(data = comb_dfs,
+                      out_file = out_file,
+                      palette = palette,
+                      default_palette = default_palette,
+                      height = out_height,
                       width = out_width)}
